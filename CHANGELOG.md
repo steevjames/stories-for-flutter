f93e6b353e543a73765e92f228fa87bb83851239
Steev James <steev.james11@gmail.com>
2025-08-10 21:12:05 +0530

    Dependency update

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,6 @@
+## 1.3.1
+Dependency update
+
 ## 1.3.0
 Use latest flutter version
 
--- lib/stories_for_flutter_method_channel.dart
+++ lib/stories_for_flutter_method_channel.dart
@@ -11,7 +11,9 @@ class MethodChannelStoriesForFlutter extends StoriesForFlutterPlatform {
 
   @override
   Future<String?> getPlatformVersion() async {
-    final version = await methodChannel.invokeMethod<String>('getPlatformVersion');
+    final version = await methodChannel.invokeMethod<String>(
+      'getPlatformVersion',
+    );
     return version;
   }
 }
